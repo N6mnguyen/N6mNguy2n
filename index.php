8f1215572a8b560fd5aa8493cc094b87889d680d
N6mnguyen <150897716+N6mnguyen@users.noreply.github.com>
2023-11-15 15:17:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head> 
+<body>
+<div class="wrapper">
+    <div class="header">
+    </div>
+    <div class="menu">
+    <ul class="list_menu">
+    <li><a href="">Trang Chu</a></li>
+    <li><a href="">Danh muc </a></li>
+    <li><a href="">Thong Ke</a></li>
+    <li><a href="">Khach Hang</a></li>
+    <li><a href="">Dang nhap</a></li>
+    </ul>
+<div id="main">
+     <div class="sidebar">
+        <p>sidebar</p>
+     </div>
+     <div class="maincontent">
+        <p>maincontent</p>
+     </div>
+ </div>
+
+    <div class="footer">
+            <p>footer</p>
+    </div>
+</div>
+    
+</body>
+</html>
